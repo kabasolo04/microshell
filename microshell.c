2e93722d285b41f8fe18dadb0b2aea4b109fd98f
Koldo Abasolo Iturregi <koldobikaabasolo@gmail.com>
2025-02-13 10:52:33 +0100

    Yippieee

--- microshell.c
+++ microshell.c
@@ -11,7 +11,7 @@ int	cd(int argc, char **cmd)
 {
 	if (argc != 2)
 		return (p("error: cd: bad arguments\n"));
-	if (chdir(cmd[0]) == -1)
+	if (chdir(cmd[0]) != 0)
 		return (p("error: cd: cannot change directory to "), p(cmd[0]), p("\n"));
 	return (0);
 }
@@ -22,8 +22,6 @@ int	exec(int argc, char **argv, char **env, int pFlag)
 	int	pid;
 	int	status;
 
-	if (!strcmp(argv[0], "cd"))
-		return (cd(argc, &argv[1]));
 	if (pFlag && pipe(fd) == -1)
 		return (p("error: fatal\n"));
 	if ((pid = fork()) == 0)
@@ -31,7 +29,10 @@ int	exec(int argc, char **argv, char **env, int pFlag)
 		if (pFlag && (dup2(fd[1], STDOUT_FILENO) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 			return (p("error: fatal\n"));
 		argv[argc] = NULL;
-		return (execve(argv[0], argv, env), p("error: cannot execute "), p(argv[0]), p("\n"));
+		if (!strcmp(argv[0], "cd"))
+			return (cd(argc, &argv[1]));
+		else
+			return (execve(argv[0], argv, env), p("error: cannot execute "), p(argv[0]), p("\n"), exit(1), 1);
 	}
 	waitpid(pid, &status, 0);
 	if (pFlag && (dup2(fd[0], STDIN_FILENO) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
@@ -44,7 +45,7 @@ int	main(int argc, char **argv, char **env)
 	int	i = 0;
 	int	status = 0;
 
-	while (argv[i++] && argv[i])
+	while (++i < argc)
 	{
 		int	j = i;
 		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
--- microshell.h
+++ microshell.h
@@ -1,20 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   microshell.h                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kabasolo <kabasolo@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/11 20:21:48 by kabasolo          #+#    #+#             */
-/*   Updated: 2025/02/12 19:42:08 by kabasolo         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef MICROSHELL_H
 # define MICROSHELL_H
 
 # include <string.h>
 # include <unistd.h>
 # include <sys/wait.h>
+# include <stdlib.h>
 
 #endif
